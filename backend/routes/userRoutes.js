6f30e725d5fedde026ad37896d5224736a2e0a19
FoxerBN <barspin4499@gmail.com>
2025-01-19 19:16:58 +0100

    fix protecded route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const {registerUser} = require('../controllers/userController/registerUser.js');
-const {loginUser} = require('../controllers/userController/loginUser.js');
-const {logoutUser} = require('../controllers/userController/logoutUser.js');
-const authenticate = require('../middlewares/auth.js');
-const validatePassword = require('../middlewares/validatePassword.js');
+const express = require("express");
+const {
+  registerUser,
+} = require("../controllers/userController/registerUser.js");
+const { loginUser } = require("../controllers/userController/loginUser.js");
+const { logoutUser } = require("../controllers/userController/logoutUser.js");
+const authenticate = require("../middlewares/auth.js");
+const validatePassword = require("../middlewares/validatePassword.js");
 
 const router = express.Router();
 
-router.post('/register', validatePassword, registerUser);
-router.post('/login', loginUser);
-router.post('/logout', authenticate, logoutUser);
-router.get('/auth', authenticate, (_, res) => {
-    return res.status(200)
-  });
+router.post("/register", validatePassword, registerUser);
+router.post("/login", loginUser);
+router.post("/logout", authenticate, logoutUser);
+router.get("/auth", authenticate, (_, res) => {
+  res.status(200).json();
+});
 
 module.exports = router;
