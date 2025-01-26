96c57ea2a94575d4656a71207e22cfc931a3fcbf
Richard Tekula <barspin4499@gmail.com>
2025-01-26 21:44:20 +0100

    modified navigation,oneDest and searhDest

--- frontend/src/pages/OneDestination.jsx
+++ frontend/src/pages/OneDestination.jsx
@@ -7,30 +7,35 @@ import DestinationsNavigation from "../components/DestinationsNavigation";
 const OneDestination = () => {
   const { id } = useParams();
   const { destinations } = useContext(DestinationsContext);
-  console.log(destinations);
 
   if (!destinations || destinations.length === 0) {
     return <p>Loading...</p>;
   }
+
   const selectedDestination = destinations.find((one) => one._id === id);
 
   if (!selectedDestination) {
     return <p>Destination not found</p>;
   }
-
+  
   return (
-    <div
-      style={{
-        backgroundImage: `url(/${selectedDestination.name}.jpg)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-      }}
-    >
-      <DestinationsNavigation />
-      <h2>{selectedDestination.name}</h2>
-      <p>{selectedDestination.rating}</p>
-    </div>
+    <>
+      
+      <div className="video-hero">
+        <DestinationsNavigation />
+        <video
+          className="video-background"
+          src={`/videos/${selectedDestination.name}.mp4`} 
+          autoPlay
+          loop
+          muted
+        />
+        <div className="hero-content">
+          <h2>{selectedDestination.name}</h2>
+          <p>{selectedDestination.rating}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
